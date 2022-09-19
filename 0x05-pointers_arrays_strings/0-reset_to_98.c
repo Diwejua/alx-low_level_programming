e7c0ec0c72ab6229d10f6cef40db21aaf2d99ca6
Daniel <iwejuadaniel@gmail.com>
2022-09-19 22:49:40 +0000

    0. 98 Battery s

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include main.h
+
+/**
+ * resect_to_98 - takes a pointer to an int as parameter and
+ * updates the value to 98
+ * @n: pointer to update to 98
+ */
+void resect_to_98(int *n)
+{
+	*n = 98;
+}
